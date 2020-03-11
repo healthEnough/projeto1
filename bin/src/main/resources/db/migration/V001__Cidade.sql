6863cdd0b8e50b3072cbb0457ce748a02830bba0
Etec 244 <e244ti@outlook.com>
2020-03-11 12:20:50 -0300

    Alteração no Cors

---
+++ bin/src/main/resources/db/migration/V001__Cidade.sql
@@ -0,0 +1,5 @@
+create table cidade(
+id int not null auto_increment primary key,
+nome varchar(100),
+estado char(2))
+ENGINE=InnoDB DEFAULT CHARSET=UTF8;
--- src/main/java/br/com/etec/projeto1/cors/CorsFilter.java
+++ src/main/java/br/com/etec/projeto1/cors/CorsFilter.java
@@ -11,6 +11,13 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
 	private String origemPermitida = "http://localhost:4200";
